39abf6a7edd89542273ac23f8ba5ae351ce218f7
Michael Zelenyuk <zell.xp@gmail.com>
2017-06-12 16:46:03 +0300

    Create a Search Bar component

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-import logo from './logo.svg';
+import ReactLogo from './logo.svg';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h2>Welcome to React</h2>
+      <div>
+        <div className="app-header">
+          <img src={ReactLogo} className="react-logo" alt="logo"/>
+          <h2>Welcome to Relab</h2>
         </div>
 
-        <p className="App-intro">
-          To get started, edit <code>src/index.js</code> and save to reload.
-        </p>
+        <div>
+          <SearchBar />
+        </div>
       </div>
     );
   }
