91e78feedf6c854d611dd48e6b4b184e5cc15d07
Michael Zelenyuk <zell.xp@gmail.com>
2017-06-13 11:25:58 +0300

    Fetch data from YouTube

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import ReactLogo from './logo.svg';
 
+const API_KEY = 'AIzaSyAuQCVeNfKhtRk9KlChQPT1nO27DPO_5Ss';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {videos: []};
+
+    YTSearch({key: API_KEY, term: 'One More Soul to the Call'}, (videos) => {
+      this.setState({videos});
+    });
+  }
+
   render() {
     return (
       <div>
