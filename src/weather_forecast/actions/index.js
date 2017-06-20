a9fc7d82e3873e21d5ce489b41bb44bb3f75247a
Michael Zelenyuk <zell.xp@gmail.com>
2017-06-20 16:46:17 +0300

    Add proxy to fix Mixed Content issue in Production

--- src/weather_forecast/actions/index.js
+++ src/weather_forecast/actions/index.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
 const API_KEY = '6a78596d062df78380eff5944c4e5567';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+// NodeJS reverse proxy which adds CORS headers to the proxied request
+// https://forum.freecodecamp.com/t/openweathermap-not-showing-on-github-pages-solved/101836
+const PROXY_URL= 'https://cors-anywhere.herokuapp.com';
+
+const ROOT_URL = `${PROXY_URL}/http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
