b61b3b3a722dcea17d95a29492b4cd6d8c1626da
Michael Zelenyuk <zell.xp@gmail.com>
2017-06-20 13:21:08 +0300

    Show last searched city at the top of table

--- src/weather_forecast/weather_forecast.jsx
+++ src/weather_forecast/weather_forecast.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Subheader from 'material-ui/Subheader';
 import SearchBar from './containers/search_bar';
 import WeatherList from './containers/weather_list';
 
@@ -6,6 +7,9 @@ class WeatherForecast extends Component {
   render() {
     return (
       <div>
+        <div className="text-center">
+          <Subheader>5 day weather forecast is only available for Ukraine</Subheader>
+        </div>
         <SearchBar/>
         <WeatherList/>
       </div>
