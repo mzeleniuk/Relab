abd751ddaba3b2cd53930af3dd82d48fa38561c1
Michael Zelenyuk <zell.xp@gmail.com>
2017-06-22 11:18:42 +0300

    Create a Posts reducer

--- src/root_reducer.js
+++ src/root_reducer.js
@@ -2,11 +2,13 @@ import {combineReducers} from 'redux';
 import BooksReducer from './books_list/reducers/books';
 import ActiveBook from './books_list/reducers/active_book';
 import WeatherReducer from './weather_forecast/reducers/weather';
+import PostsReducer from './blog/reducers/posts';
 
 const rootReducer = combineReducers({
   books: BooksReducer,
   activeBook: ActiveBook,
-  weather: WeatherReducer
+  weather: WeatherReducer,
+  posts: PostsReducer
 });
 
 export default rootReducer;
