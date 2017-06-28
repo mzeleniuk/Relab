21d012c90866da086108e633a1053f6693f878ba
Michael Zelenyuk <zell.xp@gmail.com>
2017-06-28 15:17:09 +0300

    Add ability to delete a Post

--- src/blog/components/posts_show.jsx
+++ src/blog/components/posts_show.jsx
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {fetchPost} from '../actions/index';
+import {fetchPost, deletePost} from '../actions/index';
 import CircularProgress from 'material-ui/CircularProgress';
-import {Card, CardHeader, CardText} from 'material-ui/Card';
+import {Card, CardHeader, CardText, CardActions} from 'material-ui/Card';
+import {Link} from 'react-router';
+import FlatButton from 'material-ui/FlatButton';
 
 class PostsShow extends Component {
   componentWillMount() {
     this.props.fetchPost(this.props.params.id);
+  }
+
+  onDeleteClick() {
+    this.props.deletePost(this.props.params.id);
   }
 
   render() {
@@ -25,6 +31,11 @@ class PostsShow extends Component {
         <Card>
           <CardHeader title={post.title} subtitle={'Categories: ' + post.categories}/>
           <CardText>{post.content}</CardText>
+          <CardActions>
+            <FlatButton label="Back to Index" primary={true} containerElement={<Link to="/"/>}/>
+            <FlatButton label="Delete Post" secondary={true} className="pull-right"
+                        onClick={this.onDeleteClick.bind(this)}/>
+          </CardActions>
         </Card>
       </div>
     );
@@ -35,4 +46,4 @@ function mapStateToProps(state) {
   return {post: state.posts.post}
 }
 
-export default connect(mapStateToProps, {fetchPost})(PostsShow);
+export default connect(mapStateToProps, {fetchPost, deletePost})(PostsShow);
